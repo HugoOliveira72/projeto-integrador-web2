8f015097f7b147c3548145c134f31036b271d5d2
malu <maluaraujosilva02>
2026-09-03 21:38:42 -0300

    Parte do models e evento

---
+++ models/evento.py
@@ -0,0 +1,12 @@
+#models/evento.py
+
+
+class Evento:
+
+    nome = ''
+    data = ''
+    local = ''
+    def __init__(self,nome, data, local):
+            self.nome = NotImplemented
+            self.data = data
+            self.local = local
