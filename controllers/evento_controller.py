c19f504359aa6e836bb7603c521429801990c75a
malu <maluaraujosilva02>
2026-09-03 22:00:47 -0300

    continuacao

--- controllers/evento_controller.py
+++ controllers/evento_controller.py
@@ -3,3 +3,15 @@ from models.evento import Evento
 from data.memory import eventos
 
 evento_bp = Blueprint ("evento", __name__)
+
+@evento_bp.route("/", methods = ["GET","POST"])
+def index():
+    if request.method == "POST":
+        evento = Evento
+        request.form["nome"],
+        request.form["data"],
+        request.form["local"],
+
+    eventos.append(evento)
+    return redirect("/")
+return render_template("index.html", eventos = eventos)
