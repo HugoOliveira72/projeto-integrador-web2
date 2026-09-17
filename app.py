4e5e1a1188a098f9aee5e331ecc2aa82d69f92c2
hugooliveira72 <hugo72900@gmail.com>
2026-09-17 20:24:17 -0300

    [feacture]: Fazendo com que app chame o conjunto evento

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
 from flask import Flask
+from controllers.evento_controller import evento_bp
 
 app = Flask(__name__)
+app.register_blueprint(evento_bp)
 
-@app.route('/')
-def inicio():
-    return "Hello, World!"
-
-@app.route('/sobre')
-def sobre():
-    return "Esta é a página sobre."
-
-@app.route('/evento')
-def evento():
-    return "Bem-vindo à página do evento!"
-
-#Execução de if 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+
